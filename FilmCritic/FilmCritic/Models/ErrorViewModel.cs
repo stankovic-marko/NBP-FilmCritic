550f16c4e3469ae0693b0b70308cadaae04b3d2f
Marko Stankovic <marko.98.stankovic@gmail.com>
2021-02-07 20:19:36 +0100

    Initial commit

---
+++ FilmCritic/FilmCritic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FilmCritic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
